6b9feb4759bbf64e5c1c20bb0c85bf024626a64c
Jonathan <jonathaanemanuel21@gmail.com>
2025-07-30 21:20:02 -0300

    add index loan

--- app/controllers/client/loan_controller.ts
+++ app/controllers/client/loan_controller.ts
@@ -4,12 +4,15 @@ import type { HttpContext } from '@adonisjs/core/http'
 import { updateLoanValidator, requestLoanValidator } from '#validators/client/loan_validator';
 import RequestLoanUseCase from '#use_cases/client/loan/request_loan_use_case';
 import UpdateLoanUseCase from '#use_cases/client/loan/update_loan_use_case';
+import { paginationValidator, searchValidator } from '#validators/general/index_validator';
+import LoanRepository from '#repositories/client/loan_repository';
 
 @inject()
 export default class LoanController {
     constructor(
         private readonly requestLoanUseCase: RequestLoanUseCase,
-        private readonly updateLoanUseCase: UpdateLoanUseCase
+        private readonly updateLoanUseCase: UpdateLoanUseCase,
+        private readonly loanRepository: LoanRepository
     ) { }
 
     public async request({ params, request, response }: HttpContext) {
@@ -24,5 +27,13 @@ export default class LoanController {
         const loanDto = await request.validateUsing(updateLoanValidator)
         await this.updateLoanUseCase.run(loanId, clientId, loanDto)
         return response.noContent()
+    }
+
+    public async index({ params, request, response }: HttpContext) {
+        const { clientId } = params
+        const { page = 1, perPage = 10 } = await request.validateUsing(paginationValidator)
+        const { search = '' } = await request.validateUsing(searchValidator)
+        const loans = await this.loanRepository.index(search, page, perPage, clientId)
+        return response.ok(loans)
     }
 }
